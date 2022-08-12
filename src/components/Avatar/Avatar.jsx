706a0c25d730e78964f05861f85c15c21eb53abe
Humberto Colmenares <colmenareshr@gmail.com>
2022-08-11 23:06:31 -0300

    Aplicando estilos a Avatar

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,16 +1,45 @@
-import './Avatar.css';
-
+import styled from 'styled-components';
 function Avatar() {
   return (
-      <div className='avatar'>
-        <span className='avatar__conteudo'>
+      <AvatarContainer>
+        <AvatarContent>
           <img
             src='src\assets\avatar-hc.jpg'
             alt='HC'
-            className='avatar__imagen'
           />
-        </span>
-      </div>
+        </AvatarContent>
+      </AvatarContainer>
   );
 }
 export default Avatar; 
+
+const AvatarContainer = styled.div`
+  position: relative;
+  height: 50px;
+  width: 50px;
+  margin: auto;
+`;
+
+const AvatarContent = styled.span`
+  font-size: 50px;
+  background-color: #1a73e8;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  overflow: hidden;
+  border: 2px solid #4693f6;
+
+  &:hover {
+    -webkit-box-shadow: 5px 5px 15px 5px #1a73e8;
+    box-shadow: 0px 2px 25px 2px #1a73e8;
+  }
+
+  img {
+    object-fit: cover;
+    height: 100%;
+    width: 100%;
+  }
+`;
