77f33b7a26ec5db048032f7d37b3bd750126b685
Humberto Colmenares <colmenareshr@gmail.com>
2022-08-11 22:31:45 -0300

    Unificando estilos a componente contatos

--- src/components/Contatos/Contatos.jsx
+++ src/components/Contatos/Contatos.jsx
@@ -1,20 +1,52 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClose } from '@fortawesome/free-solid-svg-icons';
-import './Contatos.css';
+import styled from 'styled-components';
 
 function Contatos({ nome, email, telefone, deleteContacts, updateContacto }) {
   return (
     <>
-      <div className='contatos_container'>
-        <div className='contatos_items_container' onClick={updateContacto}>
-          <div className='contatos_items item_nombre'>{nome}</div>
-          <div className='contatos_items item_email'>{email}</div>
-          <div className='contatos_items item_tel'>{telefone}</div>
-        </div>
+      <ContactsContainer>
+        <ContactsItemsContainer onClick={updateContacto}>
+          <ContactsItems>{nome}</ContactsItems>
+          <ContactsItems>{email}</ContactsItems>
+          <ContactsItems>{telefone}</ContactsItems>
+        </ContactsItemsContainer>
+        <div>
           <FontAwesomeIcon icon={faClose} onClick={deleteContacts} />
-      </div>
+        </div>
+      </ContactsContainer>
     </>
   );
 }
 
 export default Contatos;
+
+const ContactsContainer = styled.div`
+  background-image: linear-gradient(#fff, 57px, #fafafa, 57px);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const ContactsItemsContainer = styled.div`
+  width: 100%;
+  display: table;
+  table-layout: fixed;
+  position: relative;
+
+  &:hover {
+    background-color: #e7eeff;
+    background-size: 100% 112px;
+  }
+`;
+
+const ContactsItems = styled.div`
+  display: table-cell;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  vertical-align: middle;
+  white-space: nowrap;
+  border-bottom: 1px solid #f1f3f4;
+  padding: 20px;
+  cursor: pointer;
+`;
